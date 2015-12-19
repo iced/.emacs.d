e53dceae4966e49121b9f36dd733bed91f7f1dd9
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2015-12-19 20:02:01 +0300

    Load projectile's saved projects.

--- etc/ue-projectile.el
+++ etc/ue-projectile.el
@@ -4,6 +4,8 @@
 (setq projectile-known-projects-file (expand-file-name "projectile-bookmarks" ue-var-dir))
 (setq projectile-cache-file (expand-file-name "projectile-cache" ue-var-dir))
 
+(projectile-load-known-projects) ; due to reset of known-projects-file
+
 (projectile-global-mode)
 
 (defvar ue-projectile-enabled t)
