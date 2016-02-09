720767433e815cdcf4ac23d0a5a20bae6590155d
Andrew Kirilenko <iced@gojuno.com>
2016-02-09 13:12:36 +0300

    Better way for killing ensime customization.

--- etc/ue-scala.el
+++ etc/ue-scala.el
@@ -3,10 +3,7 @@
 (require 'scala-mode2)
 (require 'ensime)
 
-(add-hook 'scala-mode-hook (lambda ()
-                             (ensime-mode)
-                             (if (boundp 'ue-company-enabled)
-                                 (kill-local-variable 'company-idle-delay))))
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
 (setq ensime-sbt-command "sbt")
 (setq user-emacs-ensime-directory (expand-file-name "ensime" ue-var-dir))
@@ -14,7 +11,8 @@
 
 (if (boundp 'ue-company-enabled)
     (progn
-      (define-key scala-mode-map (kbd "M-TAB") #'company-complete)))
+      (define-key scala-mode-map (kbd "M-TAB") #'company-complete)
+      (add-hook 'ensime-mode-hook (lambda () (kill-local-variable 'company-idle-delay)))))
 
 (defvar ue-scala-enabled t)
 (provide 'ue-scala)
