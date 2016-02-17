d477c9dd63b330c211a1d76b19a843d642504858
Andrew Kirilenko <iced@gojuno.com>
2016-02-18 00:47:35 +0300

    Flex matching for ido.

--- etc/ue-ido.el
+++ etc/ue-ido.el
@@ -1,6 +1,7 @@
 (ido-mode t)
 (setq ido-save-directory-list-file (expand-file-name "ido" ue-var-dir))
 (setq ido-everywhere t)
+(setq ido-enable-flex-matching t)
 
 (add-to-list 'ido-ignore-buffers "\\`\\*")
 (add-to-list 'ido-ignore-files "\\`\\.")
