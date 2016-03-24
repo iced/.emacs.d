aaa7356afae7f8e01fe6e2860c1ff6fec2066d10
Andrew Kirilenko <iced@gojuno.com>
2016-03-24 13:27:57 +0300

    Adding ido-ubiquitous.

--- etc/ue-ido.el
+++ etc/ue-ido.el
@@ -1,9 +1,10 @@
-(ue-ensure-installed '(flx-ido))
+(ue-ensure-installed '(flx-ido ido-ubiquitous))
 
 (setq ido-save-directory-list-file (expand-file-name "ido" ue-var-dir))
 (ido-mode t)
 (flx-ido-mode 1)
 (ido-everywhere 1)
+(ido-ubiquitous-mode 1)
 (setq ido-enable-flex-matching t)
 
 (setq ido-enable-flex-matching t)
